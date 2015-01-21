74d5151a13fc7a7d1cd677f62382e79a0a1712c0
Vivek Rathod <vivek.rathod+oss@gmail.com>
2015-01-21 16:35:00 -0500

    sample WCF service using IReplyChannel

---
+++ WCFIReplyChannel/IReplyChannel/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading;
+
+namespace MyReplyChannel
+{
+    public class Listener
+    {
+        public static string ListenUri = "http://localhost:20025/SampleService";
+        public static Binding ListenBinding = new WSHttpBinding(SecurityMode.None);
+
+        private static int _requestCount;
+        private static int _responseCount;
+        private const int ResponseDelay = 15000; // in millisecondss
+        static void Main(string[] args)
+        {
+            IChannelListener<IReplyChannel> listener = ListenBinding.BuildChannelListener<IReplyChannel>(new Uri(ListenUri));
+            listener.Open();
+            
+            AsynchroRecv(listener);
+            //SynchroRecv(listener);
+            
+            Console.ReadKey();
+        }
+
+        static void SynchroRecv(IChannelListener<IReplyChannel> listener)
+        {
+            while (true)
+            {
+                IReplyChannel channel = listener.AcceptChannel();
+                channel.Open();
+                //Console.WriteLine("Accepted channel at {0}", DateTime.Now);
+                ThreadPool.QueueUserWorkItem(e =>
+                {
+                    while (true)
+                    {
+                        RequestContext rc = ((IReplyChannel)e).ReceiveRequest();
+                        Console.WriteLine("{0} {1} Accepted request", DateTime.Now.TimeOfDay, ++_requestCount);
+
+                        // sending a delayed response
+                        ThreadPool.QueueUserWorkItem(
+                            arg =>
+                                {
+                                    Thread.Sleep(ResponseDelay);
+                                    Console.WriteLine("{0} {1} Sending response", DateTime.Now.TimeOfDay, Interlocked.Increment(ref _responseCount));
+                                    rc.Reply(rc.RequestMessage);
+                                });
+                    }
+                }, channel);
+            }
+        }
+
+        static void AsynchroRecv(IChannelListener<IReplyChannel> listener)
+        {
+            while (true)
+            {
+                IReplyChannel channel = listener.AcceptChannel();
+                channel.Open();
+                //Console.WriteLine("Accepted channel at {0}", DateTime.Now);
+                channel.BeginReceiveRequest(ProcessRequest, channel);
+            }
+        }
+
+        static void ProcessRequest(IAsyncResult ar)
+        {
+            IReplyChannel channel = ar.AsyncState as IReplyChannel;
+            RequestContext rc = channel.EndReceiveRequest(ar);
+            Console.WriteLine("{0} {1} Accepted request", DateTime.Now.TimeOfDay, Interlocked.Increment(ref _requestCount));
+
+            // accept more requests
+            channel.BeginReceiveRequest(ProcessRequest, channel);
+
+            // sending a delayed response
+            ThreadPool.QueueUserWorkItem(e =>
+            {
+                Thread.Sleep(ResponseDelay);
+                Console.WriteLine("{0} {1} Sending response", DateTime.Now.TimeOfDay, Interlocked.Increment(ref _responseCount));
+                rc.Reply(rc.RequestMessage);
+            });
+        }
+    }
+}
---
+++ WCFIReplyChannel/SimpleClient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading;
+using MyReplyChannel;
+
+namespace SimpleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sessions = 1;
+            int calls = 4;
+            for (int i = 0; i < sessions; i++)
+            {
+                ChannelFactory<IRequestChannel> client =
+                    new ChannelFactory<IRequestChannel>(Listener.ListenBinding, Listener.ListenUri);
+                IRequestChannel channel = client.CreateChannel();
+                channel.Open();
+
+                for (int j = 0; j < calls/sessions; j++)
+                {
+                    ThreadPool.QueueUserWorkItem(
+                        e =>
+                            {
+                                try
+                                {
+                                    Message response = channel.Request(Message.CreateMessage(MessageVersion.Default, "someaction"));
+                                    Console.WriteLine("{0} Received response ", DateTime.Now.TimeOfDay);
+                                }
+                                catch (TimeoutException ex)
+                                {
+                                    Console.WriteLine("{0} Timeout {1} ", DateTime.Now.TimeOfDay, ex.Message);
+                                }
+
+                            }
+                        );
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
