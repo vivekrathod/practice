83c67238ab143847805df46bca910486b69057e1
Vivek Rathod <vrathod@trustwave.com>
2020-02-04 13:54:27 -0500

    Examples of passing parameters to actions and defining custom route

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -14,6 +14,13 @@ namespace Vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "MoviesByReleaseDate  ",
+                url: "movies/released/{year}/{month}",
+                defaults: new { controller = "Movies", action = "ByReleaseDate"},
+                new {year = @"\d{4}", month = @"\d{2}"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Ajax.Utilities;
 using Vidly.Models;
 
 namespace Vidly.Controllers
@@ -14,6 +15,27 @@ namespace Vidly.Controllers
         {
             var movie = new Movie { Id = 1, Name = "Shrek!" };
             return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id= " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (sortBy.IsNullOrWhiteSpace())
+                sortBy = "Name";
+
+            return Content("pageIndex=" + pageIndex + "&sortBy=" + sortBy);
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content("year=" + year + "&month=" + month);
         }
     }
 }
