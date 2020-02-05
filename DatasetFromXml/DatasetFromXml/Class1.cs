b7a633305dc6ece4fc4bbc6864a8cf1c3fb6680f
Vivek Rathod <vrathod@trustwave.com>
2020-02-04 21:54:04 -0500

    DataSet from XML demo

---
+++ DatasetFromXml/DatasetFromXml/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatasetFromXml
+{
+    public class Class1
+    {
+        public static void Main(string[] args)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml("ADKnowledgebase.xml");
+            ds.WriteXmlSchema(@"ADKnowledgebase.xsd");
+        }
+    }
+}
