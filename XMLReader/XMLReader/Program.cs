eec3b66920b98f59dfc64506401410a00b060c16
Vivek Rathod <vrathod@trustwave.com>
2020-02-19 11:10:11 -0500

    Using XmlReader

---
+++ XMLReader/XMLReader/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace XMLReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void Test1()
+        {
+            string xml = @"<?xml version=""1.0"" encoding=""utf-8"" ?>
+<people>
+<person firstname=""john"" lastname=""doe"">
+<contactdetails>
+<emailaddress>john@unknown.com</emailaddress>
+</contactdetails>
+</person>
+<person firstname=""jane"" lastname=""doe"">
+<contactdetails>
+<emailaddress>jane@unknown.com</emailaddress>
+<phonenumber>001122334455</phonenumber>
+</contactdetails>
+</person>
+</people>";
+            using (StringReader stringReader = new StringReader(xml))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(stringReader,
+                    new XmlReaderSettings() { IgnoreWhitespace = true }))
+                {
+                    // skip the xml declaration
+                    xmlReader.MoveToContent();
+
+                    xmlReader.ReadStartElement("people");
+
+                    string firstName = xmlReader.GetAttribute("firstname");
+                    string lastName = xmlReader.GetAttribute("lastname");
+                    Console.WriteLine("Person: {0} {1}", firstName, lastName);
+                    xmlReader.ReadStartElement("person");
+
+                    Console.WriteLine("ContactDetails");
+                    xmlReader.ReadStartElement("contactdetails");
+                    string emailAddress = xmlReader.ReadString();
+                    Console.WriteLine("Email address: {0}", emailAddress);
+                }
+            }
+        }
+
+        private static void Read(XmlReader reader)
+        {
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        Console.Write("<{0}>", reader.Name);
+                        break;
+                    case XmlNodeType.Text:
+                        Console.Write(reader.Value);
+                        break;
+                    case XmlNodeType.CDATA:
+                        Console.Write("<![CDATA[{0}]]>", reader.Value);
+                        break;
+                    case XmlNodeType.ProcessingInstruction:
+                        Console.Write("<?{0} {1}?>", reader.Name, reader.Value);
+                        break;
+                    case XmlNodeType.Comment:
+                        Console.Write("<!--{0}-->", reader.Value);
+                        break;
+                    case XmlNodeType.XmlDeclaration:
+                        Console.Write("<?xml version='1.0'?>");
+                        break;
+                    case XmlNodeType.Document:
+                        break;
+                    case XmlNodeType.DocumentType:
+                        Console.Write("<!DOCTYPE {0} [{1}]", reader.Name, reader.Value);
+                        break;
+                    case XmlNodeType.EntityReference:
+                        Console.Write(reader.Name);
+                        break;
+                    case XmlNodeType.EndElement:
+                        Console.Write("</{0}>", reader.Name);
+                        break;
+                }
+            }
+        }
+    }
+}
