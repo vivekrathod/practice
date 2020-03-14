6acf39aea143a64a2bb77fbf3835a1a9daa14a7b
Vivek Rathod <vrathod@trustwave.com>
2020-03-14 16:53:32 -0400

    Form validation

---
+++ Vidly/Vidly/Models/Min18YearsValidationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin.Security.OAuth;
+
+namespace Vidly.Models
+{
+    public class Min18YearsValidationAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var customer = validationContext.ObjectInstance as Customer;
+            if (customer.MembershipTypeId == 1)
+                return ValidationResult.Success;
+            if (customer?.BirthDate == null)
+                return new ValidationResult("Birthdate is required.");
+            var age = DateTime.Today.Year - customer.BirthDate.Value.Year;
+            return age < 18
+                ? new ValidationResult("Customer should be at least 18 years of age to become a member.")
+                : ValidationResult.Success;
+        }
+    }
+}
