ac957b9cac8db5e2b7fcefc3821614f3139b8a6f
Vivek Rathod <vrathod@trustwave.com>
2020-09-02 14:23:45 -0400

    WebSocket sample server using websocketsharp fork by sta

---
+++ WebSocketServer/WebSocketServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebSocketServer
+{
+    public class Laputa : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            var msg = e.Data == "BALUS"
+                ? "I've been balused already..."
+                : "I'm not available now.";
+
+            Send(msg);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketSharp.Server.WebSocketServer("ws://127.0.0.1");
+            wssv.AddWebSocketService<Laputa>("/Laputa");
+            wssv.Start();
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
