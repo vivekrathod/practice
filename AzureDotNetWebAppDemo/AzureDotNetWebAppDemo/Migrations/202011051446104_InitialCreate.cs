565b93699559ee75f47e15cf9101af25fa112b68
Vivek Rathod <rathod.vivek@gmail.com>
2020-11-05 09:49:00 -0500

    Enable and add initial EF database migration

---
+++ AzureDotNetWebAppDemo/AzureDotNetWebAppDemo/Migrations/202011051446104_InitialCreate.cs
@@ -0,0 +1,27 @@
+﻿namespace AzureDotNetWebAppDemo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ToDoItems",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Description = c.String(),
+                        Finished = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ToDoItems");
+        }
+    }
+}
