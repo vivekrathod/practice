fdae1735b4ef3e18f9c7ccbb30c8fd745abfbd21
Vivek Rathod <rathod.vivek@gmail.com>
2020-11-06 21:59:04 -0500

    Basic CRUD functionality for ToDo items.

--- AzureDotNetWebAppDemo/AzureDotNetWebAppDemo/Controllers/ToDoController.cs
+++ AzureDotNetWebAppDemo/AzureDotNetWebAppDemo/Controllers/ToDoController.cs
@@ -1,22 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using AzureDotNetWebAppDemo.Models;
+using AzureDotNetWebAppDemo.ViewModels;
 
 namespace AzureDotNetWebAppDemo.Controllers
 {
     public class ToDoController : Controller
     {
-        private ToDoDbContext dbContext = new ToDoDbContext();
+        private readonly ToDoDbContext _dbContext = new ToDoDbContext();
 
         // GET: ToDo
         public ActionResult Index()
         {
-            dbContext.ToDoItems.Add(new ToDoItem {Description = "Test ToDo Item 1"});
-            dbContext.SaveChanges();
+            var items = _dbContext.ToDoItems.Select(item => 
+                    new ToDoItemViewModel
+                    {
+                        Id = item.Id,
+                        Description = item.Description, 
+                        Finished = item.Finished
+                    });
+            return View(items);
+        }
+
+        public ActionResult Add()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(ToDoItemViewModel itemViewModel)
+        {
+            var items = _dbContext.ToDoItems;
+            if (ModelState.IsValid)
+            {
+                var item = new ToDoItem
+                    {
+                        Description = itemViewModel.Description, 
+                        Finished = itemViewModel.Finished
+                    }
+                    ;
+                items.Add(item);
+                _dbContext.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var item = _dbContext.ToDoItems.Find(id);
+            if (item == null)
+                return HttpNotFound();
+
+            ToDoItemViewModel itemViewModel = new ToDoItemViewModel
+            {
+                Id = id,
+                Description = item.Description,
+                Finished = item.Finished
+            };
+
+            return View("Edit", itemViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ToDoItemViewModel itemViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = _dbContext.ToDoItems.Find(itemViewModel.Id);
+                if (item != null)
+                {
+                    item.Description = itemViewModel.Description;
+                    item.Finished = itemViewModel.Finished;
+                }
+
+                _dbContext.SaveChanges();
+            }
+
+            return View(itemViewModel);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            var item = _dbContext.ToDoItems.Find(id);
+            if (item == null)
+                return HttpNotFound();
+
+            var itemViewModel = new ToDoItemViewModel
+            {
+                Id = item.Id, 
+                Description = item.Description, 
+                Finished = item.Finished
+            };
+            return View(itemViewModel);
+        }
+
+        [HttpPost, ActionName("Delete"), ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var item = _dbContext.ToDoItems.Find(id);
+            if (item == null)
+                return HttpNotFound();
+
+            _dbContext.ToDoItems.Remove(item);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ AzureDotNetWebAppDemo/AzureDotNetWebAppDemo/ViewModels/ToDoItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AzureDotNetWebAppDemo.Models;
+
+namespace AzureDotNetWebAppDemo.ViewModels
+{
+    public class ToDoItemViewModel
+    {
+        public int? Id { get; set; }
+        public string Description { get; set; }
+        public bool Finished { get; set; }
+    }
+}
