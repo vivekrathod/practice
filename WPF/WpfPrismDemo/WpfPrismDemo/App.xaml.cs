8627b4f6f8800a172bea2f50db25991192bd5614
Vivek Rathod <rathod.vivek@gmail.com>
2021-02-05 23:07:10 -0500

    Wpf Prism "Hello World!"

---
+++ WPF/WpfPrismDemo/WpfPrismDemo/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfPrismDemo.Views;
+
+namespace WpfPrismDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<ShellWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
