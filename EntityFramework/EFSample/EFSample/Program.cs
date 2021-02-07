ab0951ea4873dc7c438c6d290b3d33a40e10fcd1
Vivek Rathod <rathod.vivek@gmail.com>
2021-02-07 11:31:58 -0500

    Additional examples

--- EntityFramework/EFSample/EFSample/Program.cs
+++ EntityFramework/EFSample/EFSample/Program.cs
@@ -14,28 +14,34 @@ namespace EFSample
             Console.WriteLine("Hello World!");
             using (var db = new BlogContext())
             {
-                // Create
-                //Console.WriteLine("Inserting a new blog");
-                //db.Blogs.Add(new Blog { Name = "My Blog!", Url = "http://blog.vivekrathod.com/" });
-                //db.SaveChanges();
+                InitializeData(db, 2, 3);
+
+                // log EF SQL to Console
+                db.Database.Log = Console.Write;
 
                 // Read
                 Console.WriteLine("Querying for a blog");
-                var blog = db.Blogs
-                    //.Include("Posts")
-                    .OrderBy(b => b.Id)
-                    .First();
+                //var blog = db.Blogs
+                //    //.Include("Posts") // because Lazy loading is turned ON
+                //    .OrderBy(b => b.Id)
+                //    .First();
 
-                //All Posts
-                //var posts = blog.Posts.Where(p => p.Name == "Hello World 2").ToList();
-                var posts = db.Blogs
-                    .Where(b=>b.Posts.Any(p=>p.Name == "Hello World 2"))
-                    //.SelectMany(b => b.Posts)
-                    ;
-                foreach (var post in posts)
-                {
-                    Console.WriteLine($"Post: {post.Name}");
-                }
+                // example 1
+                var posts = db.Blogs.Where(blog => blog.Posts.Where(post => post.Name.ToLower().Contains("hello")).Any()).ToList();
+
+                // example 2
+                var blogs2 = db.Blogs
+                    .SelectMany(b => b.Posts.Where(p => p.Name.ToLower().Contains("hello")))
+                    //.Select(p => new { Blog = p.Blog, Post = p })
+                    .Select(a => a.Blog)
+                    .ToList();
+                
+                // example 3
+                var blogs3 = db.Blogs
+                    .SelectMany(b => b.Posts.Where(p => p.Name.ToLower().Contains("hello")))
+                    .Select(p => new { Blog = p.Blog, Post = p })
+                    .ToList()
+                    .Select(a => a.Blog);
 
                 // Update
                 //Console.WriteLine("Updating the blog and adding a post");
@@ -48,11 +54,38 @@ namespace EFSample
                 //    });
                 //db.SaveChanges();
 
-                // Delete
-                //Console.WriteLine("Delete the blog");
-                //db.Blogs.Remove(blog);
-                //db.SaveChanges();
+                ClearData(db);
             }
+        }
+
+        static void InitializeData(BlogContext db, int noOfBlogs, int noOfPosts)
+        {
+            // Create
+            for (int b = 0; b < noOfBlogs; b++)
+            {
+                Console.WriteLine($"Inserting a blog {b}");
+                var blog = new Blog { Name = $"My Blog {b}!", Url = $"http://blog{b}.vivekrathod.com/" };
+                for (int p = 0; p < noOfPosts; p++)
+                {
+                    var post = new Post
+                    {
+                        Name = $"Hello World {p}",
+                        Content = $"I wrote an app using EF Core {p}!"
+                    };
+                    blog.Posts.Add(post);
+                }
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+        }
+
+        static void ClearData(BlogContext db)
+        {
+            // Delete
+            Console.WriteLine("Delete all blogs and posts");
+            db.Database.ExecuteSqlCommand("delete from posts");
+            db.Database.ExecuteSqlCommand("delete from blogs");
+            db.SaveChanges();
         }
     }
 }
